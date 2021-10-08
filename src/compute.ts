3585673ee9f1b9ba797078cf67611c0bd51bc443
SibianDG <sibiandegussem@outlook.com>
2021-10-08 16:11:44 +0200

    Implementing checks and first computation

--- src/compute.ts
+++ src/compute.ts
@@ -1,5 +1,50 @@
 import { Game } from "./types";
+import {run} from "jest";
 
 export function compute(game: Game): number {
-  throw new Error("Not yet implemented"); // TODO
+  let score: number = 0;
+  game.forEach((turn: Array<number>, index: number) => {
+    if (index < 9){
+      score += turn[0] + turn[1];
+      if (isStrike(turn)){
+        if (index+1 < 9)
+          score += calculateScoreTurn(game[index+1]);
+        if (index+2 < 9)
+          score += calculateScoreTurn(game[index+2]);
+      } else if(isSpare(turn) ) {
+        score += game[index + 1][0];
+      }
+    } else if (index === 9) {
+      score += turn[0] + turn[1] + turn[2];
+      if (turn[0] === 10){
+        score += turn[1] + turn[2]
+      }
+      if (turn[1] === 10){
+        score += turn[2]
+      } else {
+        score += turn[2]
+      }
+    }
+    console.log(`Score ${score} op index ${index}`)
+  })
+
+  return score;
 }
+
+function isSpare(turn: Array<number>): boolean {
+  return (turn[0] + turn[1]) === 10;
+}
+
+function isStrike(turn: Array<number>): boolean {
+  return turn[0] === 10 || turn[1] === 10;
+}
+
+function calculateScoreTurn(turn: Array<number>): number {
+  if (turn.length === 3)
+    return turn[0]+turn[1]+turn[2];
+  else
+    return turn[0]+turn[1];
+}
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,37 @@
 import http from "http";
 import express from "express";
 import { compute } from "./compute";
+import {Game} from "./types";
 
 export const app = express();
 
 app.use(express.json());
 
 app.post("/compute", (request, response) => {
-  const game = request.body.game;
-  // TODO: Validate input
+  const gameRequest = request.body.game;
+  let game: Game;
+  try {
+    game = gameRequest;
+    if (game.length !== 10)
+      throw new Error('Bad request')
+    gameRequest.forEach((e: Array<number>, i: number) => {
+      console.log(e + " --- " + i)
+      if (i < 9) {
+        if (e.length !== 2)
+          throw new Error('Bad request')
+      } else if (i === 9){
+        if (e.length < 2 || e.length > 3)
+          throw new Error('Bad request')
+      } else {
+        throw new Error('Bad request')
+      }
+    })
+  } catch (error) {
+    response.status(400)
+    response.send(error.message);
+  }
 
   const score = compute(game);
-
-  // TODO: Return response
+  response.status(200)
+  response.send({"score": score});
 });
