d17d4f1af535ce78b0719daa7e92e27c2df5a3f1
SibianDG <sibiandegussem@outlook.com>
2021-10-09 12:18:29 +0200

    Added some tests

--- tests/compute.test.ts
+++ tests/compute.test.ts
@@ -19,3 +19,42 @@ it("should return 300 on a perfect game", () => {
 
   expect(score).toBe(300);
 });
+
+
+it("should return 150 on a 5-5 game", () => {
+  const input150: Game = [
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5],
+    [5, 5, 5],
+  ];
+
+  const score = compute(input150);
+
+  expect(score).toBe(150);
+});
+
+it("should return 90 on a 9 - game", () => {
+  const input90: Game = [
+    [9, 0],
+    [9, 0],
+    [9, 0],
+    [9, 0],
+    [9, 0],
+    [9, 0],
+    [9, 0],
+    [9, 0],
+    [9, 0],
+    [9, 0, 0],
+  ];
+
+  const score = compute(input90);
+
+  expect(score).toBe(90);
+});
